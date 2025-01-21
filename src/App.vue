99628ff620d27978f86b12f5d3bb2ffb2653c246
kouhei <kouhei.basketball7.8@gmail.com>
2025-01-21 14:37:11 +0900

    first commit

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <main>
+    <h2>今日何食べる</h2>
+  </main>
+</template>
+
+<style scoped>
+  main {
+    width: 350px;
+    margin: auto;
+    text-align: center;
+  }
+</style>
