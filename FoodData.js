5962f51774c873bc9b0062bd1e21dda75783e85a
kouhei <kouhei.basketball7.8@gmail.com>
2025-01-22 15:41:56 +0900

    add:食べ物のダミーデータ作成

---
+++ FoodData.js
@@ -0,0 +1,38 @@
+export const FoodData = [
+  {
+    name: 'カレー',
+    emoji: '🍛',
+  },
+  {
+    name: '寿司',
+    emoji: '🍣',
+  },
+  {
+    name: '肉',
+    emoji: '🍖',
+  },
+  {
+    name: 'ラーメン',
+    emoji: '🍜',
+  },
+  {
+    name: 'ハンバーガー',
+    emoji: '🍔',
+  },
+  {
+    name: '居酒屋',
+    emoji: '🍺',
+  },
+  {
+    name: 'おでん',
+    emoji: '🍢',
+  },
+  {
+    name: '鍋',
+    emoji: '🍲',
+  },
+  {
+    name: 'どんぶり系',
+    emoji: '🍚',
+  }
+];
