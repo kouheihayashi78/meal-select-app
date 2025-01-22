cd62e08a5c7cb80fa6577e66e7751f716ad427b9
kouhei <kouhei.basketball7.8@gmail.com>
2025-01-22 15:44:17 +0900

    add:親コンポーネントでダミーデータをプロップスとして渡したり、デザインを整えたりして、ざっくり実装

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,18 @@
 <script setup lang="ts">
+import { ref } from 'vue';
+import FoodBlock from "./components/FoodBlock.vue";
+import { FoodData } from "../FoodData.js";
+
+const foodList = ref(FoodData);
 </script>
 
 <template>
   <main>
-    <h2>今日何食べる</h2>
+    <h2 class="title">今日、何食べたい？？？</h2>
+
+    <FoodBlock :name="foodList[0].name" :emoji="foodList[0].emoji" />
+    <p>vs</p>
+    <FoodBlock :name="foodList[1].name" :emoji="foodList[1].emoji"  />
   </main>
 </template>
 
@@ -12,5 +21,9 @@
     width: 350px;
     margin: auto;
     text-align: center;
+  }
+  .title {
+    font-size: 40px;
+    margin-bottom: 50px;
   }
 </style>
