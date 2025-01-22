a9d1abc9a50b9713d1104ad7918ba5f1659c846a
kouhei <kouhei.basketball7.8@gmail.com>
2025-01-22 16:52:50 +0900

    add:フードブロックをクリックすると、クリックしたフードを上に表示し、下にダミーデータからランダムな値を表示する処理実装

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,37 @@
 <script setup lang="ts">
 import { ref } from 'vue';
 import FoodBlock from "./components/FoodBlock.vue";
-import { FoodData } from "../FoodData.js";
+import type { Food } from '../FoodData.ts';
+import { FoodData } from '../FoodData.ts';
 
-const foodList = ref(FoodData);
+const foodList = ref<Food[]>(FoodData)
+
+const pickUpFoodIndex = ref<number>(0)
+const newFoodIndex = ref<number>(1)
+
+const pickUpFood = (index: number): void => {
+  pickUpFoodIndex.value = index
+  newFoodIndex.value++
+}
 </script>
 
 <template>
   <main>
     <h2 class="title">今日、何食べたい？？？</h2>
 
-    <FoodBlock :name="foodList[0].name" :emoji="foodList[0].emoji" />
-    <p>vs</p>
-    <FoodBlock :name="foodList[1].name" :emoji="foodList[1].emoji"  />
+    <FoodBlock
+      :name="foodList[pickUpFoodIndex].name"
+      :emoji="foodList[pickUpFoodIndex].emoji"
+      @click="pickUpFood(pickUpFoodIndex)"
+    />
+    <template v-if="newFoodIndex < foodList.length">
+      <p>vs</p>
+      <FoodBlock
+        :name="foodList[newFoodIndex].name"
+        :emoji="foodList[newFoodIndex].emoji"
+        @click="pickUpFood(newFoodIndex)"
+      />
+    </template>
   </main>
 </template>
 
